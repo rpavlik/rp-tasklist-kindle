be90aa820ca7a4e447402f233de0d4831f920f35
Ryan Pavlik <ryan.pavlik@collabora.com>
2021-02-25 13:18:08 -0600

    OK, this part seems good for now

--- gulpfile.js
+++ gulpfile.js
@@ -1,26 +1,28 @@
 
-const { src, dest, series } = require("gulp");
+const { src, dest, parallel } = require("gulp");
 const nunjucksRender = require('gulp-nunjucks-render');
-const rename = require("gulp-rename");
-const data = require('gulp-data'); var fs = require('fs');
+const rename = require("gulp-rename");var fs = require('fs');
 const cleanDir = require('gulp-clean-dir');
+const zip = require('gulp-zip');
 
 
 const templatesDir = 'src/templates';
-const templates = `${templatesDir}/*.njk`;
+const scriptTemplates = `${templatesDir}/{Install,Uninstall}/*.njk`;
+const appTemplates = `${templatesDir}/app/{**/}*.njk`;
 const outDir = './dist';
 
 function getData() {
     return JSON.parse(fs.readFileSync('./src/data.json'));
 }
 
+const loadedData = getData();
+
 function cleanTask(cb) {
     cleanDir(outDir);
     cb();
 }
-function templateTask() {
-    const loadedData = getData();
-    return src(templates)
+function scriptTemplatesTask() {
+    return src(scriptTemplates)
         // Renders template with nunjucks
         .pipe(nunjucksRender({
             path: [templatesDir],
@@ -35,5 +37,17 @@ function templateTask() {
         .pipe(dest(outDir))
 }
 
-exports.default = series(templateTask);
+function appTemplatesTask() {
+    return src(appTemplates)
+        // Renders template with nunjucks - no renaming
+        .pipe(nunjucksRender({
+            path: [templatesDir],
+            ext: '',
+            data: loadedData
+        }))
+        // output files in app folder
+        .pipe(dest(outDir))
+}
+
+exports.default = parallel(scriptTemplatesTask, appTemplatesTask);
 exports.clean = cleanTask;
