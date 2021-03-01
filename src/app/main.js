ff4e06f8b010d94e04d2850a23496311fbcbe6eb
Ryan Pavlik <ryan.pavlik@gmail.com>
2021-02-28 21:56:25 -0600

    wip

--- src/app/main.js
+++ src/app/main.js
@@ -1,20 +1,9 @@
-import 'whatwg-fetch'
-import { init } from './hnl.mobileConsole.js'
-
-import $ from "jquery";
-
 
 function doStuffOnLoad() {
     if (typeof kindle !== 'undefined') {
 
         $("#a").innerHTML = "this is a kindle";
         kindle.dev.log("this is a kindle");
-        // this is a kindle
-        init();
-        alert("kindle")
-    } else {
-        document.getElementById("a").innerHTML = "not a kindle";
-        // alert("not kindle")
     }
 
     $("mine").onclick = function () {
@@ -44,28 +33,3 @@ if (typeof Kindle !== 'undefined') {
         b.applicationManager.addEventListener('go', onLoad);
     })
 }
-// $(function() {
-
-// })
-
-// window.onload = () => {
-//     if (typeof kindle !== 'undefined') {
-
-//         document.getElementById("a").innerHTML = "this is a kindle"
-//         // this is a kindle
-//         init();
-//     } else {
-//         document.getElementById("a").innerHTML = "not a kindle"
-//     }
-
-//     document.getElementById("mine").onclick = () => {
-//         document.getElementById("stuff").innerHTML = "plz wait";
-//         fetch('http://192.168.1.150:1880/office/tasks')
-//             .then(function (response) {
-//                 return response.json();
-//             }).then(function (json) {
-//                 document.getElementById("stuff").innerHTML = json;
-//             });
-//         return false;
-//     };
-// };
