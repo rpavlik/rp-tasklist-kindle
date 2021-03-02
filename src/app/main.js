74abbf47df673fe3cf447f214505ff32aa0d45c9
Ryan Pavlik <ryan.pavlik@gmail.com>
2021-03-01 21:33:16 -0600

    Remove some dead code

--- src/app/main.js
+++ src/app/main.js
@@ -1,24 +1,6 @@
 
 'use strict';
-// function doStuffOnLoad() {
-//     if (typeof kindle !== 'undefined') {
 
-//         $("#b").innerHTML = "this is a kindle";
-//         kindle.dev.log("this is a kindle");
-//     }
-
-//     $("mine").onclick = function () {
-//         $("#stuff").innerHTML = "plz wait";
-//         // document.getElementById("stuff").innerHTML =
-//         fetch('http://192.168.1.150:1880/office/tasks')
-//             .then(function (response) {
-//                 return response.json();
-//             }).then(function (json) {
-//                 $("#stuff").innerHTML = json;
-//             });
-//         return false;
-//     };
-// }
 // if (typeof Kindle !== 'undefined') {
 //     Kindle.use(function (b) {
 //         var logger = b.logging.createEventLogger('RYAN');
