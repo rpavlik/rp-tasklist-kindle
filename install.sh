2c50ca9ff46879fbae18cfeb7aa1f6f756baa979
Ryan Pavlik <ryan.pavlik@collabora.com>
2021-03-02 10:49:20 -0600

    launch after install

--- install.sh
+++ install.sh
@@ -6,4 +6,7 @@ ADDRESS=${ADDRESS:-192.168.1.51}
     npm run-script build
     echo "Installing on ${ADDRESS}"
     scp -r dist/extensions/rptasklist root@${ADDRESS}:/mnt/us/extensions/
+
+    echo "Launching"
+    ssh root@${ADDRESS} /mnt/us/extensions/rptasklist/bin/start.sh
 )
